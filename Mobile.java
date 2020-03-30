e96c6d5f40b178b04f3313c7d4005532ac289214
ramparani <61373592+ramparani@users.noreply.github.com>
2020-03-30 22:18:33 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Mobile {
+//short age=11;
+boolean own=true;
+int modelnum=12345;
+String brand="Iphone";
+	public static void main(String[] args) {
+		
+System.out.println("am ready");
+Mobile mob=new Mobile();
+boolean own2 = mob.own;
+int modelnum2 = mob.modelnum;
+String brand2=mob.brand;
+System.out.println(own2);
+System.out.println(modelnum2);
+System.out.println(brand2);
+	}
+
+}
